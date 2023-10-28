e804431894960a15e59d395859710441152ea3e1
Fred Youhanaie <fyrlang@anydata.co.uk>
2023-10-28 02:28:43 +0100

    first commit

---
+++ src/wfnet.erl
@@ -0,0 +1,11 @@
+%%%-------------------------------------------------------------------
+%%% @author Fred Youhanaie <fyrlang@anydata.co.uk>
+%%% @copyright 2021, Fred Youhanaie
+%%% @doc
+%%%
+%%% @end
+%%% Created :  2023-10-28 by Fred Youhanaie <fyrlang@anydata.co.uk>
+%%%-------------------------------------------------------------------
+-module(wfnet).
+
+-export([]).
